c7f0a44d1e966017a665835e70891792306fa8cb
Admin <Admin@SALMAAMRO>
2025-11-13 12:09:55 +0200

    Implementation DAL

---
+++ Company.Route.DAL/Data/Contexts/CompanyDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Company.Route.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+
+namespace Company.Route.DAL.Data.Contexts
+{
+    internal class CompanyDbContext:DbContext
+    {
+        // Constructor To open Connection to Database
+        public CompanyDbContext():base()
+        {
+
+        }
+        // to configure the model
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
+        // Open Connection to Database
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Srver:.;Database = CompanyProjct;Trusted_Connection = True;TrustedServerCertificate=True");
+        }
+
+        // Map Models to Database Tables
+        public DbSet<Department> Departments { get; set; }
+    }
+}
